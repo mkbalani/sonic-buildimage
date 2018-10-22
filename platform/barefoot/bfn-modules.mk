d17f693817c383a072f789e22aafbbb877ca55a1
mkbalani <mk_sagarbalani@yahoo.com>
2018-10-22 16:07:23 -0700

    Change debian pkg name for bfn modules

--- platform/barefoot/bfn-modules.mk
+++ platform/barefoot/bfn-modules.mk
@@ -2,7 +2,7 @@
 
 VERSION = 1.0
 
-BFN_MODULE = platform-modules-bfn_$(VERSION)_amd64.deb
+BFN_MODULE = bfn-modules_$(VERSION)_amd64.deb
 $(BFN_MODULE)_SRC_PATH = $(PLATFORM_PATH)/bfn-modules
 $(BFN_MODULE)_DEPENDS += $(LINUX_HEADERS) $(LINUX_HEADERS_COMMON)
 SONIC_DPKG_DEBS += $(BFN_MODULE)
